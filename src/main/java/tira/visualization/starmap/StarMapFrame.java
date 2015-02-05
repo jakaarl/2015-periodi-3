ca386b72561b7517a25f06c703afdb2ddd051aeb
Jani Kaarela <jani.kaarela@saunalahti.com>
2015-02-05 23:36:14 +0200

    Beginning visualization tool.

---
+++ src/main/java/tira/visualization/starmap/StarMapFrame.java
@@ -0,0 +1,70 @@
+package tira.visualization.starmap;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Insets;
+import java.awt.Shape;
+import java.awt.geom.Ellipse2D;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import tira.domain.Star;
+import tira.navigation.NavigationNode;
+import tira.navigation.StarMap;
+
+@SuppressWarnings("serial")
+public class StarMapFrame extends JFrame {
+    
+    private static final String TITLE = "Star Map";
+    
+    StarMapFrame(StarMap starMap) {
+        super();
+        setTitle(TITLE);
+        setExtendedState(MAXIMIZED_BOTH);
+        add(new StarMapPanel(starMap));
+    }
+    
+    private static class StarMapPanel extends JPanel {
+        
+        private StarMap starMap;
+        private int xOffset;
+        private int yOffset;
+        
+        public StarMapPanel(StarMap starMap) {
+            super(true);
+            this.starMap = starMap;
+            setBackground(Color.BLACK);
+        }
+        
+        public void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            Dimension size = getSize();
+            Insets insets = getInsets();
+            xOffset = (size.width - (insets.left + insets.right)) / 2;
+            yOffset = (size.height - (insets.top + insets.bottom)) / 2;
+            paintStars(graphics);
+        }
+        
+        private void paintStars(Graphics graphics) {
+            Graphics2D g2d = (Graphics2D) graphics;
+            g2d.setColor(Color.WHITE);
+            for (NavigationNode node : starMap.stars) {
+                paintStar(node.star, g2d);
+            }
+        }
+        
+        private void paintStar(Star star, Graphics2D g2d) {
+            int radius = 10;
+            int x = star.location.x + xOffset - (radius / 2);
+            int y = star.location.y + yOffset - (radius / 2);
+            Shape circle = new Ellipse2D.Float(x, y, radius, radius);
+            g2d.draw(circle);
+            g2d.fillOval(x, y, radius, radius);
+        }
+        
+    }
+
+}
---
+++ src/main/java/tira/visualization/starmap/StarMapLauncher.java
@@ -0,0 +1,48 @@
+package tira.visualization.starmap;
+
+import java.util.Arrays;
+
+import javax.swing.SwingUtilities;
+
+import tira.input.JsonStarListReader;
+import tira.navigation.StarMap;
+
+/**
+ * Launcher for a {@link StarMap} visualization. By default, looks for the file
+ * {@value #DEFAULT_STARMAP_PATH} (relative to working directory) or the file
+ * specified as an argument. Maximum &quot;jump distance&quot; between stars
+ * defaults to {@value #DEFAULT_MAP_JUMP_DISTANCE}, but can be set with the
+ * system property {@value #MAX_JUMP_DISTANCE_KEY}.
+ *
+ */
+public class StarMapLauncher {
+    
+    public static final String MAX_JUMP_DISTANCE_KEY = "maxJumpDistance";
+    public static final int DEFAULT_MAP_JUMP_DISTANCE = 750;
+    public static final String DEFAULT_STARMAP_PATH = "starmap.json";
+    
+    public static void main(String[] args) {
+        String starMapPath;
+        if (args.length == 0) {
+            starMapPath = DEFAULT_STARMAP_PATH;
+        } else if (args.length == 1) {
+            starMapPath = args[0];
+        } else {
+            throw new IllegalArgumentException(
+                    "Illegal arguments (0 or 1 arguments expected): " + Arrays.deepToString(args));
+        }
+        int maxDistance = Integer.valueOf(
+                System.getProperty(MAX_JUMP_DISTANCE_KEY, String.valueOf(DEFAULT_MAP_JUMP_DISTANCE)));
+        JsonStarListReader reader = new JsonStarListReader();
+        final StarMap map = reader.buildStarMap(starMapPath, maxDistance);
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                StarMapFrame frame = new StarMapFrame(map);
+                frame.setVisible(true);
+            }
+        });
+        
+    }
+
+}
